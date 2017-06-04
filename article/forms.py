9e59fcaae9d57db10948e4958827721322537a73
VitaliyVoronov <vitalik.voronov@gmail.com>
2017-06-04 16:37:36 +0300

    first commit

---
+++ article/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from article.models import Comments
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comments
+        fields = ['comments_text']
+
+
+
---
+++ loginsys/views.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.template.context_processors import csrf
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Create your views here.
+def login(request):
+    args = {}
+    args.update(csrf(request))
+    if request.POST:
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            args['login_error'] = "Username or password is not correct"
+            return render(request, 'login.html', args)
+    else:
+        return render(request, 'login.html', args)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+def register(request):
+    args = {}
+    args.update(csrf(request))
+    args['form'] = UserCreationForm()
+    if request.POST:
+        newuser_form = UserCreationForm(request.POST)
+        if newuser_form.is_valid():
+            newuser_form.save()
+            newuser = auth.authenticate(username=newuser_form.cleaned_data['username'],
+                                        password=newuser_form.cleaned_data['password2'])
+            auth.login(request, newuser)
+            return redirect('/')
+        else:
+            args['form'] = newuser_form
+    return render(request, 'register.html', args)
